7859c583f0da4e11f988eb266479731b92990989
Mateus <mrbarreto@proton.me>
2025-07-06 14:20:46 -0300

    Refatoração da classe Agendamento para record para reduzir código boilerplate

--- src/servicos/agendamento/Agendamento.java
+++ src/servicos/agendamento/Agendamento.java
@@ -5,37 +5,8 @@ import entidades.Usuario;
 
 import java.time.LocalDateTime;
 
-public class Agendamento {
-
-	private final LocalDateTime dataInicio;
-	private final LocalDateTime dataFim;
-	private final Usuario usuario;
-	private final EspacoFisico espaco;
-
-	public Agendamento(LocalDateTime dataInicio, LocalDateTime dataFim, Usuario usuario, EspacoFisico espaco) {
-		this.dataInicio = dataInicio;
-		this.dataFim = dataFim;
-		this.usuario = usuario;
-		this.espaco = espaco;
-	}
-
-	public LocalDateTime getDataInicio() {
-		return dataInicio;
-	}
-
-	public LocalDateTime getDataFim() {
-		return dataFim;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public EspacoFisico getEspaco() {
-		return espaco;
-	}
-
+public record Agendamento(LocalDateTime dataInicio, LocalDateTime dataFim, Usuario usuario, EspacoFisico espaco) {
 	public boolean sobrepoe(LocalDateTime inicio, LocalDateTime fim) {
-		return !fim.isBefore(this.dataInicio) && !inicio.isAfter(this.dataFim);
+		return !fim.isBefore(dataInicio) && !inicio.isAfter(dataFim);
 	}
 }
