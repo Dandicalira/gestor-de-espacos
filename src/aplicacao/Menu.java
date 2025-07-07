3e12df85b17f2b346d7ad3ede61d448dcf42800a
Daniel <daniellira0009@gmail.com>
2025-07-06 21:12:11 -0300

    Adicionado protótipo de Menu

--- src/aplicacao/Menu.java
+++ src/aplicacao/Menu.java
@@ -1,6 +1,30 @@
 package aplicacao;
 
 public class Menu {
-
 	
+	public static void main(String[] args) {
+		Menu a = new Menu();
+		a.imprimirMenuInicial();
+	}
+	
+	//Métodos Construtores
+	public Menu() {
+		
+	}
+	
+	
+	
+	public void menuInicial() {
+		
+		imprimirMenuInicial();
+	}
+	
+	private void imprimirMenuInicial() {
+		
+		System.out.print("--- Sistema de Gestão de Espaços Físicos da Universidade ---\n"
+			       + "Escolha um módulo para acessá-lo:\n"
+			       + "1 - Cadastro de Usuários e Espaços Físicos (senha de Superusuário necessária)\n"
+			       + "2 - Login como Usuário para serviços como Agendamentos e Emissão de Relatório\n"
+			       + "3 - Sair do sistema\n");
+	}
 }
